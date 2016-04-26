6c32ec6240e7426b9d009b4c10ddcb6eb6a5d3e8
mastromac <mr.aquaro@gmail.com>
2016-04-26 23:40:17 +0200

    some changes

--- src/main/java/it/uniroma2/lmp/humansvszombies/gui/GameView.java
+++ src/main/java/it/uniroma2/lmp/humansvszombies/gui/GameView.java
@@ -10,189 +10,170 @@ import java.util.Map;
 
 /**
  *
- * Rappresentazione grafica della griglia del campo.
- * Questa view visualizza le location come dei rettangoli colorati in base
- * al contenuto della location.
+ * Rappresentazione grafica della griglia del campo. Questa view visualizza le
+ * location come dei rettangoli colorati in base al contenuto della location.
  *
  */
-public class GameView extends JFrame
-{
-    // Colore per le caselle vuote
-    private static final Color EMPTY_COLOR = Color.white;
+public class GameView extends JFrame {
+	// Colore per le caselle vuote
+	private static final Color EMPTY_COLOR = Color.white;
 
-    // Colore usato per gli oggetti non definiti
-    private static final Color UNKNOWN_COLOR = Color.gray;
+	// Colore usato per gli oggetti non definiti
+	private static final Color UNKNOWN_COLOR = Color.gray;
 
-    private final String STEP_PREFIX = "Step: ";
-    private final String POPULATION_PREFIX = "Pedine: ";
-    private JLabel stepLabel, population;
-    private FieldView fieldView;
-    
-    // Una mappa che memorizza i colori delle pedine del gioco
-    private HashMap colors;
-    // Campo per la statistica
-    private FieldStats stats;
+	private final String STEP_PREFIX = "Step: ";
+	private final String POPULATION_PREFIX = "Pedine: ";
+	private JLabel stepLabel, population;
+	private FieldView fieldView;
 
-    /**
-     * Crea la visuale del campo da gioco
-     * @param height altezza del campo
-     * @param width larghezza del campo
-     */
-    public GameView(int height, int width)
-    {
-        stats = new FieldStats();
-        colors = new HashMap();
+	// Una mappa che memorizza i colori delle pedine del gioco
+	private HashMap colors;
+	// Campo per la statistica
+	private FieldStats stats;
 
-        setTitle("Humans vs Zombies");
-        stepLabel = new JLabel(STEP_PREFIX, JLabel.CENTER);
-        population = new JLabel(POPULATION_PREFIX, JLabel.CENTER);
-        
-        setLocation(100, 50);
-        
-        fieldView = new FieldView(height, width);
+	/**
+	 * Crea la visuale del campo da gioco
+	 * 
+	 * @param height
+	 *            altezza del campo
+	 * @param width
+	 *            larghezza del campo
+	 */
+	public GameView(int height, int width) {
+		stats = new FieldStats();
+		colors = new HashMap();
 
-        Container contents = getContentPane();
-        contents.add(stepLabel, BorderLayout.NORTH);
-        contents.add(fieldView, BorderLayout.CENTER);
-        contents.add(population, BorderLayout.SOUTH);
-        pack();
-        setVisible(true);
-    }
-    
+		setTitle("Humans vs Zombies");
+		stepLabel = new JLabel(STEP_PREFIX, JLabel.CENTER);
+		population = new JLabel(POPULATION_PREFIX, JLabel.CENTER);
 
-    public void setColor(Class actorClass, Color color)
-    {
-        colors.put(actorClass, color);
-    }
-    
+		setLocation(100, 50);
 
-    public void setColors(Map colorMap)
-    {
-        colors.putAll(colorMap);
-    }
+		fieldView = new FieldView(height, width);
 
+		Container contents = getContentPane();
+		contents.add(stepLabel, BorderLayout.NORTH);
+		contents.add(fieldView, BorderLayout.CENTER);
+		contents.add(population, BorderLayout.SOUTH);
+		pack();
+		setVisible(true);
+	}
 
-    private Color getColor(Class actorClass)
-    {
-        Color col = (Color)colors.get(actorClass);
-        if(col == null) {
-            return UNKNOWN_COLOR;
-        }
-        else {
-            return col;
-        }
-    }
+	public void setColor(Class actorClass, Color color) {
+		colors.put(actorClass, color);
+	}
 
-    /**
-     * Visulizza lo stato corrente del campo
-     * @param turn Turno di gioco
-     * @param field Campo da gioco corrente.
-     */
-    public void showStatus(int turn, Field field)
-    {
-        if(!isVisible())
-            setVisible(true);
+	public void setColors(Map colorMap) {
+		colors.putAll(colorMap);
+	}
 
-        stepLabel.setText(STEP_PREFIX + turn);
+	private Color getColor(Class actorClass) {
+		Color col = (Color) colors.get(actorClass);
+		if (col == null) {
+			return UNKNOWN_COLOR;
+		} else {
+			return col;
+		}
+	}
 
-        stats.reset();
-        fieldView.preparePaint();
-            
-        for(int row = 0; row < field.getDepth(); row++) {
-            for(int col = 0; col < field.getWidth(); col++) {
-                Object actor = field.getObjectAt(row, col);
-                if(actor != null) {
-                    stats.incrementCount(actor.getClass());
-                    fieldView.drawMark(col, row, getColor(actor.getClass()));
-                }
-                else {
-                    fieldView.drawMark(col, row, EMPTY_COLOR);
-                }
-            }
-        }
-        stats.countFinished();
+	/**
+	 * Visulizza lo stato corrente del campo
+	 * 
+	 * @param turn
+	 *            Turno di gioco
+	 * @param field
+	 *            Campo da gioco corrente.
+	 */
+	public void showStatus(int turn, Field field) {
+		if (!isVisible())
+			setVisible(true);
 
-        population.setText(POPULATION_PREFIX + stats.getActorDetails(field));
-        fieldView.repaint();
-    }
+		stepLabel.setText(STEP_PREFIX + turn);
 
-    /**
-     * Determina se la simulazione deve continuare o no
-     * @return true se ci sono ancora delle pedine attive
-     */
-    public boolean isViable(Field field)
-    {
-        return stats.isViable(field);
-    }
-    
-    /**
-     * Questa classe fornisce una rappresentazione grafica di una casella
-     * del campo da gioco.
-     */
-    private class FieldView extends JPanel
-    {
-        private final int GRID_VIEW_SCALING_FACTOR = 6;
+		stats.reset();
+		fieldView.preparePaint();
 
-        private int gridWidth, gridHeight;
-        private int xScale, yScale;
-        Dimension size;
-        private Graphics g;
-        private Image fieldImage;
+		for (int row = 0; row < field.getDepth(); row++) {
+			for (int col = 0; col < field.getWidth(); col++) {
+				Object actor = field.getObjectAt(row, col);
+				if (actor != null) {
+					stats.incrementCount(actor.getClass());
+					fieldView.drawMark(col, row, getColor(actor.getClass()));
+				} else {
+					fieldView.drawMark(col, row, EMPTY_COLOR);
+				}
+			}
+		}
+		stats.countFinished();
 
+		population.setText(POPULATION_PREFIX + stats.getActorDetails(field));
+		fieldView.repaint();
+	}
 
-        public FieldView(int height, int width)
-        {
-            gridHeight = height;
-            gridWidth = width;
-            size = new Dimension(0, 0);
-        }
+	/**
+	 * Determina se la simulazione deve continuare o no
+	 * 
+	 * @return true se ci sono ancora delle pedine attive
+	 */
+	public boolean isViable(Field field) {
+		return stats.isViable(field);
+	}
 
+	/**
+	 * Questa classe fornisce una rappresentazione grafica di una casella del
+	 * campo da gioco.
+	 */
+	private class FieldView extends JPanel {
+		private final int GRID_VIEW_SCALING_FACTOR = 6;
 
-        public Dimension getPreferredSize()
-        {
-            return new Dimension(gridWidth * GRID_VIEW_SCALING_FACTOR,
-                                 gridHeight * GRID_VIEW_SCALING_FACTOR);
-        }
-        
+		private int gridWidth, gridHeight;
+		private int xScale, yScale;
+		Dimension size;
+		private Graphics g;
+		private Image fieldImage;
 
-        public void preparePaint()
-        {
-            if(! size.equals(getSize())) {  // if the size has changed...
-                size = getSize();
-                fieldImage = fieldView.createImage(size.width, size.height);
-                g = fieldImage.getGraphics();
+		public FieldView(int height, int width) {
+			gridHeight = height;
+			gridWidth = width;
+			size = new Dimension(0, 0);
+		}
 
-                xScale = size.width / gridWidth;
-                if(xScale < 1) {
-                    xScale = GRID_VIEW_SCALING_FACTOR;
-                }
-                yScale = size.height / gridHeight;
-                if(yScale < 1) {
-                    yScale = GRID_VIEW_SCALING_FACTOR;
-                }
-            }
-        }
-        
+		public Dimension getPreferredSize() {
+			return new Dimension(gridWidth * GRID_VIEW_SCALING_FACTOR, gridHeight * GRID_VIEW_SCALING_FACTOR);
+		}
 
-        public void drawMark(int x, int y, Color color)
-        {
-            g.setColor(color);
-            g.fillRect(x * xScale, y * yScale, xScale-1, yScale-1);
-        }
+		public void preparePaint() {
+			if (!size.equals(getSize())) { // if the size has changed...
+				size = getSize();
+				fieldImage = fieldView.createImage(size.width, size.height);
+				g = fieldImage.getGraphics();
 
+				xScale = size.width / gridWidth;
+				if (xScale < 1) {
+					xScale = GRID_VIEW_SCALING_FACTOR;
+				}
+				yScale = size.height / gridHeight;
+				if (yScale < 1) {
+					yScale = GRID_VIEW_SCALING_FACTOR;
+				}
+			}
+		}
 
-        public void paintComponent(Graphics g)
-        {
-            if(fieldImage != null) {
-                Dimension currentSize = getSize();
-                if(size.equals(currentSize)) {
-                    g.drawImage(fieldImage, 0, 0, null);
-                }
-                else {
-                    // Rescale the previous image.
-                    g.drawImage(fieldImage, 0, 0, currentSize.width, currentSize.height, null);
-                }
-            }
-        }
-    }
+		public void drawMark(int x, int y, Color color) {
+			g.setColor(color);
+			g.fillRect(x * xScale, y * yScale, xScale - 1, yScale - 1);
+		}
+
+		public void paintComponent(Graphics g) {
+			if (fieldImage != null) {
+				Dimension currentSize = getSize();
+				if (size.equals(currentSize)) {
+					g.drawImage(fieldImage, 0, 0, null);
+				} else {
+					// Rescale the previous image.
+					g.drawImage(fieldImage, 0, 0, currentSize.width, currentSize.height, null);
+				}
+			}
+		}
+	}
 }
--- src/main/java/it/uniroma2/lmp/humansvszombies/playground/ActorGenerator.java
+++ src/main/java/it/uniroma2/lmp/humansvszombies/playground/ActorGenerator.java
@@ -1,5 +1,6 @@
 package it.uniroma2.lmp.humansvszombies.playground;
 
+import it.uniroma2.lmp.humansvszombies.actors.HuberZombie;
 import it.uniroma2.lmp.humansvszombies.actors.Human;
 import it.uniroma2.lmp.humansvszombies.actors.Zombie;
 
@@ -14,48 +15,50 @@ import java.util.Random;
  * Questa classe si occupa di popolare il campo con gli attori del sistema
  *
  */
-public class ActorGenerator
-{ 
+public class ActorGenerator {
 
+	private Map colorMap;
 
-    private Map colorMap;
+	public ActorGenerator() {
+		colorMap = new HashMap();
+		colorMap.put(Human.class, Color.BLUE);
+		colorMap.put(Zombie.class, Color.ORANGE);
+		colorMap.put(HuberZombie.class, Color.RED);
+	}
 
-    public ActorGenerator()
-    {        
-        colorMap = new HashMap();
-        colorMap.put(Human.class, Color.BLUE);
-        colorMap.put(Zombie.class, Color.ORANGE);
-    }
-       
-    /**
-     * Popola il campo con gli attori.
-     * @param field Il campo che deve essere popolato.
-     */
-    public void populate(Field field, List humans, List zombies)
-    {
-        Random rand = new Random();
-        field.clear();
-        for(int row = 0; row < field.getDepth(); row++) {
-            for(int col = 0; col < field.getWidth(); col++) {
-                if(rand.nextDouble() <= 0.2) {
-                    Human human = new Human();
-                    human.setLocation(row, col);
-                    humans.add(human);
-                    field.place(human);
-                }
-                else if(rand.nextDouble() <= 0.5) {
-                    Zombie zombie = new Zombie();
-                    zombie.setLocation(row, col);
-                    zombies.add(zombie);
-                    field.place(zombie);
-                }
-            }
-        }
-    }
+	/**
+	 * Popola il campo con gli attori.
+	 * 
+	 * @param field
+	 *            Il campo che deve essere popolato.
+	 */
+	public void populate(Field field, List humans, List zombies, List huberZombies) {
+		Random rand = new Random();
+		field.clear();
+		for (int row = 0; row < field.getDepth(); row++) {
+			for (int col = 0; col < field.getWidth(); col++) {
+				if (rand.nextDouble() <= 0.2) {
+					Human human = new Human();
+					human.setLocation(row, col);
+					humans.add(human);
+					field.place(human);
+				} else if (rand.nextDouble() <= 0.5) {
+					Zombie zombie = new Zombie();
+					zombie.setLocation(row, col);
+					zombies.add(zombie);
+					field.place(zombie);
+				} else if (rand.nextDouble() <= 0.8) {
+					HuberZombie huberZombie = new HuberZombie();
+					huberZombie.setLocation(row, col);
+					huberZombies.add(huberZombie);
+					field.place(huberZombie);
+				}
+			}
+		}
+	}
 
+	public Map getColors() {
+		return colorMap;
+	}
 
-    public Map getColors() {
-        return colorMap;
-    }
-    
 }
