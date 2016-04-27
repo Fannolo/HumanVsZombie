c74363d969872b327412ee0e58aa07d2a7010eda
mastromac <mr.aquaro@gmail.com>
2016-04-27 15:06:51 +0200

    prova

--- src/main/java/it/uniroma2/lmp/humansvszombies/utils/FieldStats.java
+++ src/main/java/it/uniroma2/lmp/humansvszombies/utils/FieldStats.java
@@ -91,6 +91,7 @@ public class FieldStats {
 	 * @return true se la partita deve continaure
 	 */
 
+	
 	private boolean isViableWithMunitionHuberZombie() {
 		Counter infoMunition = (Counter) counters.get(Munition.class);
 		Counter infoHuman = (Counter) counters.get(Human.class);
